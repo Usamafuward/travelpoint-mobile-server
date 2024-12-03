1bf0bd72c426dce4826791b87ab4c5aad850dede
Umair Fuward <usamafuward2001@gmail.com>
2024-12-04 04:28:44 +0530

    latest changes

--- app/routers/equipments.py
+++ app/routers/equipments.py
@@ -18,6 +18,7 @@ endpoint_errors = {
 async def create_equipment(
     owner_id: int = Form(...),
     type: str = Form(...),
+    location: str = Form(...),
     description: Optional[str] = Form(None),
     photo: Optional[UploadFile] = None,
 ):
@@ -29,8 +30,8 @@ async def create_equipment(
             
     # Prepare and execute database query
     query = """
-    INSERT INTO equipments (Owner_id, type, description, photo_path)
-    VALUES (%s, %s, %s, %s) RETURNING id
+    INSERT INTO equipments (Owner_id, type, description, photo_path, location)
+    VALUES (%s, %s, %s, %s, %s) RETURNING id
     """
     
     try:
@@ -41,6 +42,7 @@ async def create_equipment(
                 type,
                 description,
                 photo_path,
+                location,
             ),
         )
         equipment_id = cur.fetchone()
@@ -227,23 +229,20 @@ async def delete_equipment(equipment_id: int):
         )
 
 
-@router.get("/equipment/status/{user_id}", responses=endpoint_errors)
-async def get_equipment_status(user_id: int):
+@router.get("/equipment/status/{owner_id}", responses=endpoint_errors)
+async def get_equipment_status(owner_id: int):
     try:
         query = """
         SELECT status FROM equipments
-        WHERE user_id = %s
+        WHERE owner_id = %s
         ORDER BY created_at DESC
         LIMIT 1;
         """
-        cur.execute(query, (user_id,))
+        cur.execute(query, (owner_id,))
         result = cur.fetchone()
 
         if not result:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail="No guide requests found for the user",
-            )
+            return {"status": 0}
         return {"status": result["status"]}
     except Exception as e:
         print(f"ERROR - DB:\n{e}")
